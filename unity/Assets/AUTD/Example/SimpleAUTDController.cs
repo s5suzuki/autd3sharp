32bb295cfa07c04e333d4d91fbb21014824164f3
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2021-06-29 23:36:14 +0900

    v1.4.3

--- unity/Assets/AUTD/Example/SimpleAUTDController.cs
+++ unity/Assets/AUTD/Example/SimpleAUTDController.cs
@@ -4,7 +4,7 @@
  * Created Date: 08/03/2021
  * Author: Shun Suzuki
  * -----
- * Last Modified: 23/05/2021
+ * Last Modified: 29/06/2021
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2021 Hapis Lab. All rights reserved.
@@ -27,11 +27,10 @@ public class SimpleAUTDController : MonoBehaviour
         _autd.AddDevice(gameObject.transform.position, gameObject.transform.rotation);
 
         string ifname = @"write your interface name here";
-        _link = Link.SOEMLink(ifname, _autd.NumDevices);
+        _link = Link.SOEM(ifname, _autd.NumDevices);
         _autd.Open(_link);
 
         _autd.Clear();
-        _autd.Synchronize();
 
         _autd.Send(Modulation.Sine(150)); // 150 Hz
     }
